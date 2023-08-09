1a4558df2d75ebc8e3bd6b2b167c2d36f665ea8c
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-08-09 12:40:34 +0600

    add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,15 +15,9 @@ export class HeroesComponent {
 
   heroes:Hero[]=[];
 
-  constructor(private heroService:HeroService,private messageService:MessageService){
+  constructor(private heroService:HeroService){
   }
 
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
